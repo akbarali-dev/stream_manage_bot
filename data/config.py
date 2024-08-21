a706f23b5d67cc62491837e393f06f93fb70a14c
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2024-08-21 22:20:42 +0500

    remove ip

--- data/config.py
+++ data/config.py
@@ -6,7 +6,6 @@ env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-IP = env.str("ip")  # Xosting ip manzili
 
 DB_USER = env.str("POSTGRES_USER")
 DB_PASS = env.str("POSTGRES_PASSWORD")

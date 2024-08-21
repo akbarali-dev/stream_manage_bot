597e6b8060804dbdd766ff84bbc8b3b9c46371e5
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2024-08-21 22:17:41 +0500

    change database

--- data/config.py
+++ data/config.py
@@ -8,7 +8,7 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 IP = env.str("ip")  # Xosting ip manzili
 
-DB_USER = env.str("DB_USER")
-DB_PASS = env.str("DB_PASS")
-DB_NAME = env.str("DB_NAME")
-DB_HOST = env.str("DB_HOST")
+DB_USER = env.str("POSTGRES_USER")
+DB_PASS = env.str("POSTGRES_PASSWORD")
+DB_NAME = env.str("POSTGRES_DB")
+DB_HOST = env.str("POSTGRES_HOST")
